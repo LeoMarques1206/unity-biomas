ef8b65532097662a859c58d8df2ae0530086d5b7
Leonardo Marques <leogmarq@gmail.com>
2024-04-13 12:38:03 -0300

    Commit 13/04 12:37
    Commit com caixa de texto implementada

---
+++ Biomas/Assets/ActivateText.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateText : MonoBehaviour
+{
+    public GameObject dialogue;
+     private Dialogue dialogueScript;
+
+    void Start()
+    {
+        
+        dialogueScript = dialogue.GetComponent<Dialogue>();
+       
+        if (dialogueScript == null)
+        {
+            Debug.LogError("DialogueScript não encontrado no objeto com a tag 'Player'");
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            dialogueScript.gameObject.SetActive(true);
+            dialogueScript.StartDialogue();
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(dialogue);
+        }
+    }
+}
