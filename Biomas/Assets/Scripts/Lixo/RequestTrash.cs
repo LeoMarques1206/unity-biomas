0855c6400851ef3dc8190d1a1b31ba5819c247ab
Leonardo Marques <leogmarq@gmail.com>
2024-06-11 22:33:12 -0300

    CommitFinal

--- Biomas/Assets/Scripts/Lixo/RequestTrash.cs
+++ Biomas/Assets/Scripts/Lixo/RequestTrash.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.PackageManager.Requests;
+//using UnityEditor.PackageManager.Requests;
 using System;
 
 public class RequestTrash : MonoBehaviour
